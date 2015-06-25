9c20c7ae928e3403f752c9f514206f4a1c2e4547
Gabriel Gatzsche <gabriel.gatzsche@audanika.com>
2015-06-25 11:38:05 +0200

    Changed copyright string.

--- testA/testA/mainTestA.m
+++ testA/testA/mainTestA.m
@@ -3,7 +3,7 @@
 //  testA
 //
 //  Created by Gabriel Gatzsche on 25.06.15.
-//  Copyright © 2015 Gatzsche. All rights reserved.
+//  Copyright © 2015 Audanika. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
